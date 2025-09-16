d1c345cacde8ad5a72a8998454ae0350b0457955
Alex Malyk <alex.s.malyk@gmail.com>
2025-09-16 14:47:16 +0200

    create DontDestroyOnLoad utility script

---
+++ Assets/Scripts/Runtime/Utils/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TowerDefence.Runtime.Utils
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
