262a3561f40ba7074196d557880c12e5fdb196e3
Alex Malyk <alex.s.malyk@gmail.com>
2025-09-16 13:59:24 +0200

    create abstract class IdentifiableScriptableObject which has readable name and guid

---
+++ Assets/Scripts/Runtime/Data/IdentifiableScriptableObject.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+namespace TowerDefence.Runtime.Data
+{
+    [Serializable]
+    public abstract class IdentifiableScriptableObject : ScriptableObject
+    {
+        [Header("Identity")] 
+        [SerializeField] private string _displayName;
+        [SerializeField] private string _guidString;
+
+        private Guid? _cachedGuid;
+
+        public string DisplayName => _displayName;
+
+        public Guid Id
+        {
+            get
+            {
+                if (_cachedGuid.HasValue) return _cachedGuid.Value;
+                
+                if (string.IsNullOrEmpty(_guidString))
+                {
+                    _guidString = Guid.NewGuid().ToString();
+#if UNITY_EDITOR
+                    UnityEditor.EditorUtility.SetDirty(this);
+#endif
+                }
+
+                _cachedGuid = Guid.Parse(_guidString);
+
+                return _cachedGuid.Value;
+            }
+        }
+
+        protected virtual void OnValidate()
+        {
+            if (!string.IsNullOrEmpty(_guidString)) return;
+            
+            _guidString = Guid.NewGuid().ToString();
+        }
+
+        public bool HasId(Guid id)
+        {
+            return Id.Equals(id);
+        }
+    }
+}
