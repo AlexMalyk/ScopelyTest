0340f2d6af137f3e3e883542856fb8a9f98cf20e
Alex Malyk <alex.s.malyk@gmail.com>
2025-09-19 16:05:13 +0100

    update turret config

--- Assets/Scripts/Runtime/Battle/Turrets/TurretConfig.cs
+++ Assets/Scripts/Runtime/Battle/Turrets/TurretConfig.cs
@@ -1,9 +1,15 @@
 using TowerDefence.Runtime.Config;
+using UnityEngine;
 
 namespace TowerDefence.Runtime.Battle.Turrets
 {
     public class TurretConfig : IdentifiableConfig
     {
-
+        [Header("Turret Config")]
+        [SerializeField] private GameObject _prefab;
+        [SerializeField] private int _cost;
+        
+        public GameObject Prefab => _prefab;
+        public int Cost => _cost;
     }
 }
