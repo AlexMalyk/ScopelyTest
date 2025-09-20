b607308ada9427e33ec92ecd0bdf82070de8829c
Alex Malyk <alex.s.malyk@gmail.com>
2025-09-20 17:02:12 +0100

    create simple economy system

---
+++ Assets/Scripts/Runtime/Battle/Economy/GoldSystem.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace TowerDefence.Runtime.Battle.Economy
+{
+    public class GoldSystem : MonoBehaviour
+    {
+        private double _goldAmount;
+
+        public double GoldAmount => _goldAmount;
+
+        public Action<double> OnGoldAmountChanged;
+        
+        public void SpendGold(double amount)
+        {
+            _goldAmount -= amount;
+            OnGoldAmountChanged?.Invoke(_goldAmount);
+        }
+
+        public void AddGold(double amount)
+        {
+            _goldAmount += amount;
+            OnGoldAmountChanged?.Invoke(_goldAmount);
+        }
+
+        public bool CanAfford(double amount) =>
+            _goldAmount >= amount;
+    }
+}
