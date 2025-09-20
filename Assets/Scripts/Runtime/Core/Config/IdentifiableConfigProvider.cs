8487796c42f3078955ff9083a5d55baac28f14ad
Alex Malyk <alex.s.malyk@gmail.com>
2025-09-20 17:04:35 +0100

    optimize lookup with dictionary

--- Assets/Scripts/Runtime/Core/Config/IdentifiableConfigProvider.cs
+++ Assets/Scripts/Runtime/Core/Config/IdentifiableConfigProvider.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using VContainer;
 
 namespace TowerDefence.Runtime.Config
@@ -6,24 +7,28 @@ namespace TowerDefence.Runtime.Config
     public class IdentifiableConfigProvider<T> where T : IdentifiableConfig
     {
         private readonly T[] _configs;
+        private readonly Dictionary<Guid, T> _configLookup;
+        
+        public int Count => _configs.Length;
 
         [Inject]
         public IdentifiableConfigProvider(T[] configs)
         {
             _configs = configs;
+            _configLookup = new Dictionary<Guid, T>();
+        
+            foreach (var config in configs)
+            {
+                if (config != null)
+                    _configLookup[config.Id] = config;
+            }
         }
-
-        public int Count => _configs.Length;
 
         public T GetByGuid(Guid guid)
         {
-            foreach (var t in _configs)
-                if (t.Id == guid)
-                    return t;
-
-            return null;
+            return _configLookup.GetValueOrDefault(guid);
         }
-
+        
         public T GetRandom()
         {
             return _configs[UnityEngine.Random.Range(0, _configs.Length)];
