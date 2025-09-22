adf5860eb8c8cbf4f7a12cd7f1aa07a4ff057bca
Alex Malyk <alex.s.malyk@gmail.com>
2025-09-22 01:00:10 +0100

    update components

--- Assets/Scripts/Runtime/Battle/Configs/ProjectileConfig.cs
+++ Assets/Scripts/Runtime/Battle/Configs/ProjectileConfig.cs
@@ -6,13 +6,6 @@ namespace TowerDefence.Runtime.Battle.Configs
     [CreateAssetMenu(fileName = "ProjectileConfig", menuName = "Configs/Projectile")]
     public class ProjectileConfig : IdentifiableConfig
     {
-        [Header("Projectile Settings")]
-        [SerializeField] private float _baseDamage = 10f;
-        [SerializeField] private float _baseSpeed = 10f;
-        [SerializeField] private float _lifeTime = 5f;
-        
-        public float BaseDamage => _baseDamage;
-        public float BaseSpeed => _baseSpeed;
-        public float LifeTime => _lifeTime;
+
     }
 }
